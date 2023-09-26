021fd8f2654ed2146887fe90e52d5bd6ccdffe76
shreyanshishukla <shreyashi1217@gmail.com>
2023-09-26 14:07:54 +0530

    created a react app from scratch using bable transpiler

---
+++ build/index.js
@@ -0,0 +1,4 @@
+const ele = React.createElement("div", {}, "hiii running");
+ReactDOM.render(ele, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root2"));
+root.render("helllo again");
